f5e34123e93e62a94a226b79e1738f69ecee55cd
Sebastian Große <sebastian.grosse91@gmail.com>
2017-05-19 23:12:13 +0200

    first working version

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Simple command line game to try out docker with node.js
 Build the docker image and run the container in interactive mode to start the game.
 
 docker build . -t numberguessing
+
 docker run -it --rm numberguessing
--- index.js
+++ index.js
@@ -6,25 +6,84 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-console.info(`This is a number guessing game.\nOnly absolute numbers are allowed!`);
-console.info(`\nQuit with ctrl-c`);
-
-rl.question(`Tell me the limit for the random number? `, function(answer) {
-    console.log(`You have chosen ${answer} as your upper limit!`);
+rl.on('SIGINT', () => {
+    rl.question('Are you sure you want to exit the game? ', (answer) => {
+        if (answer.match(/^y(es)?$/i)) rl.close();
+    });
 });
 
-function parseInput(input) {
-    return Number.parseInt(input, 10);
+var game = {
+    name: '',
+    limit: 0,
+    random: 0,
+    guess: 0
 }
 
-function guess() {
-    rl.question(`What's your guess? `, function (guess) {
-        var g = parseInput(guess);
-        console.log(`You guessed ${g}`);
+const askName =  () => {
+    rl.question(`What's your name? `, (answer) => {
+        console.info(`Hello ${answer}`);
+        game.name = answer;
+        askLimit();
     });
 }
 
-function generateRandom(max) {
-    var m = Math.floor(max);
-    return Math.floor(Math.random()*m);
+const askLimit = () => {
+    rl.question(`Hey ${game.name}, What should be the upper limit for the number? `, (answer) => {
+        answer = Number.parseInt(answer, 10);
+        if (Number.isNaN(answer)){
+            console.info(`This is not a number. Try again!`);
+            askLimit();
+        } else {
+            console.info(`${answer} sounds good, I will generate a random number for you!`);
+            game.limit = answer;
+            game.random = generateRandom(game.limit);
+            askGuess();
+        }
+    });
 }
+
+const generateRandom = (limit) => {
+    return Number.parseInt(Math.floor(Math.random() * limit), 10);
+}
+
+const askGuess = () => {
+    rl.question(`What's your guess? `, (answer) => {
+        answer = Number.parseInt(answer, 10);
+        if (Number.isNaN(answer)){
+            console.info(`This is not a number. Try again!`);
+            askGuess();
+        } else {
+            game.guess = answer;
+            console.info(`You guessed ${game.guess}`);
+            evaluateGuess();
+        }
+    });
+}
+
+const evaluateGuess = () => {
+    if (game.random === game.guess) {
+        console.info(`You Win!`);
+        endGame();
+    } else if (game.random > game.guess) {
+        console.info(`Too low!`);
+        askGuess();
+    } else if (game.random < game.guess) {
+        console.info(`Too high!`);
+        askGuess();
+    } else {
+        console.info(`Unexpected result ${game.guess} and ${game.random}`);
+        console.info(`Typeof: ${typeof game.guess} and ${typeof game.random}`);
+    }
+}
+
+const endGame = () => {
+    rl.question(`Play again? `, (answer) => {
+        if (answer.match(/^y(es)?$/i)) {
+            askLimit();
+        } else {
+            rl.close();
+        }
+    });
+}
+
+askName();
